c5c8e6b639db3e4d58a4f44819df3cd025a9ce73
Glitch (glitch-hello-website) <none>
2021-08-20 13:20:04 +0000

    🚴📙 Checkpoint ./index.php:34681680/374 ./style.css:34681680/293

--- index.php
+++ index.php
@@ -10,58 +10,63 @@
     <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
   </head>
   <body>
-    <p>Telkフォーラム</p>
-    <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-      名前 <input type="text" name="personal_name" required><br><br>
-      <p>投稿内容 </p>
-      <textarea name="contents" required></textarea><br><br>
-      <input type="submit" name="btn1" value="投稿する">
-    </form>
-    <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    writeData();
-}
-readData();
-function readData(){
-    $cl_file = 'cl.txt';
-    $fp = fopen($cl_file, 'rb');
-    if ($fp){
-        if (flock($fp, LOCK_SH)){
-            while (!feof($fp)) {
-                $buffer = fgets($fp);
-                print($buffer);
-            }
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
-        }
-    }
-    fclose($fp);
-}
-function writeData(){
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-    $data = $data."<div class='post'><p>投稿者:".$personal_name."</p>\r\n";
-    $data = $data."<div class='solid'></div>\r\n";
-    $data = $data."<p>".$contents."</p>\r\n</div>";
-    $keijban_file = 'cl.txt';
-    $fp = fopen($keijban_file, 'ab');
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
-            }
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
-        }
-    }
-    fclose($fp);
-}
-    ?>
-    <div class="solid"></div>
-    <p>容量の問題もあるので、ここに書かれたコメントは予告なく削除される場合もあります</p>
-    <p>©Copyright BellMe 2020-2021 All Right Reserved.</p>
+    <div class="main">
+      <p>Telkフォーラム</p>
+      <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+        名前 <input type="text" name="personal_name" required><br><br>
+        <p>投稿内容 </p>
+        <textarea name="contents" required></textarea><br><br>
+        <input type="submit" name="btn1" value="投稿する">
+      </form>
+    </div>
+    <div class="wake"></div>
+    <div class="sub">
+      <?php
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+      writeData();
+  }
+  readData();
+  function readData(){
+      $cl_file = 'cl.txt';
+      $fp = fopen($cl_file, 'rb');
+      if ($fp){
+          if (flock($fp, LOCK_SH)){
+              while (!feof($fp)) {
+                  $buffer = fgets($fp);
+                  print($buffer);
+              }
+              flock($fp, LOCK_UN);
+          }else{
+              print('ファイルロックに失敗しました');
+          }
+      }
+      fclose($fp);
+  }
+  function writeData(){
+      $personal_name = $_POST['personal_name'];
+      $contents = $_POST['contents'];
+      $contents = nl2br($contents);
+      $data = $data."<div class='post'><p>投稿者:".$personal_name."</p>\r\n";
+      $data = $data."<div class='solid'></div>\r\n";
+      $data = $data."<p>".$contents."</p>\r\n</div>";
+      $keijban_file = 'cl.txt';
+      $fp = fopen($keijban_file, 'ab');
+      if ($fp){
+          if (flock($fp, LOCK_EX)){
+              if (fwrite($fp,  $data) === FALSE){
+                  print('ファイル書き込みに失敗しました');
+              }
+              flock($fp, LOCK_UN);
+          }else{
+              print('ファイルロックに失敗しました');
+          }
+      }
+      fclose($fp);
+  }
+      ?>
+      <div class="solid"></div>
+      <p>容量の問題もあるので、ここに書かれたコメントは予告なく削除される場合もあります</p>
+      <p>©Copyright BellMe 2020-2021 All Right Reserved.</p>
+    </div>
   </body>
 </html>
