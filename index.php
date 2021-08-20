5f34d18486a8a270ea6726ecb79a9f673497e2a3
Glitch (glitch-hello-website) <none>
2021-08-20 09:19:46 +0000

    😂☄️ Checkpoint ./style.css:34681680/797 ./index.php:34681680/1146

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
     <link rel="stylesheet" href="https://jaow.glitch.me/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;500&display=swap" rel="stylesheet">
   </head>
   <body>
     <script src="https://jaow.glitch.me/script.js"></script>
-    <p class="center h1">WELCOME TO JAOW.</p><br>
-    <p class="side h2">ABOUT JAOW</p><br>
-    <p>JAOW is very simple JavaScript library.</p><br>
-    <p class="side h3">How to use JAOW's scripts</p><br>
-    <p class="side h2">ALL SCRIPTS</p><br>
+    <h1 class="center">Simple</h1>
+    <h2>ABOUT JAOW</h2>
+    <p>JAOW is very simple JavaScript library.</p>
+    <h3>How to use JAOW's scripts</h3>
+    <h2>ALL SCRIPTS</h2>
     <p>現在のスクリプト数：</p><br>
   </body>
 </html>
