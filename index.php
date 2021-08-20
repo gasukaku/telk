f80604c1283f55f11785e806aae554406afee7d5
Glitch (glitch-hello-website) <none>
2021-08-20 07:38:07 +0000

    🍝🎓 Checkpoint ./index.php:34681680/591 ./style.css:34681680/819

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
     <link rel="stylesheet" href="https://jaow.glitch.me/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
   </head>
   <body>
     <script src="https://jaow.glitch.me/script.js"></script>
-    <h1>WEL COME TO JAOW.</h1>
-    <h2>JAOWについて</h2>
+    <h1 class="center bigger">WELCOME TO JAOW.</h1>
+    <h2 class="side">ABOUT JAOW</h2>
     <p>
     JAOWはとてもシンプルなJavaScriptライブラリです。<p></p>
     JAOWの文をインストールする場合は、txtファイルですので、拡張子を.jsにしてご使用ください。<p></p>
     </p>
     <div class="main">
-    <h2>一覧</h2>
+    <h2 class="side">ALL</h2>
       <p>
         簡略化：
         <p>roop文：<a href="https://jaow.glitch.me/funcs/js/ss/roop/script.js">リンクに移動</a>　<a href="https://jaow.glitch.me/funcs/js/ss/roop/package.txt" download>インストール</a>　<a href="https://jaow.glitch.me/funcs/js/ss/roop/index.html">説明</a></p>
