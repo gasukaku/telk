d25d97d562e0a843a29d13d7e566f543d6114487
Glitch (glitch-hello-website) <none>
2021-08-20 09:53:55 +0000

    👲⚓️ Checkpoint ./index.php:34681680/1137 ./index.html:34681680/6388 ./style.css:34681680/706

--- index.php
+++ index.php
@@ -1,75 +1,67 @@
 
 <html>
-<head><title>Telk</title></head>
-<body>
-
-<p>Telk chat</p>
-
-<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-<input type="text" name="personal_name"><br><br>
-<textarea name="contents" rows="8" cols="40">
-</textarea><br><br>
-<input type="submit" name="btn1" value="send">
-</form>
-
-<?php
-
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <title>Telk</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://telk.glitch.me/style.css">
+    <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
+  </head>
+  <body>
+    <p>Telk chat</p>
+    <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+      名前 <input type="text" name="personal_name"><br><br>
+      <p>投稿内容 </p>
+      <textarea name="contents" rows="8" cols="40">
+      </textarea><br><br>
+      <input type="submit" name="btn1" value="Send">
+    </form>
+    <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     writeData();
 }
-
 readData();
-
 function readData(){
     $keijban_file = 'keijiban.txt';
-
     $fp = fopen($keijban_file, 'rb');
-
     if ($fp){
         if (flock($fp, LOCK_SH)){
             while (!feof($fp)) {
                 $buffer = fgets($fp);
                 print($buffer);
             }
-
             flock($fp, LOCK_UN);
         }else{
             print('ファイルロックに失敗しました');
         }
     }
-
     fclose($fp);
 }
-
 function writeData(){
     $personal_name = $_POST['personal_name'];
     $contents = $_POST['contents'];
     $contents = nl2br($contents);
-
-    $data = "<hr>\r\n";
+    $data = $data."<div class='post'>";
     $data = $data."<p>投稿者:".$personal_name."</p>\r\n";
     $data = $data."<p>内容:</p>\r\n";
     $data = $data."<p>".$contents."</p>\r\n";
-
+    &
     $keijban_file = 'keijiban.txt';
-
     $fp = fopen($keijban_file, 'ab');
-
     if ($fp){
         if (flock($fp, LOCK_EX)){
             if (fwrite($fp,  $data) === FALSE){
                 print('ファイル書き込みに失敗しました');
             }
-
             flock($fp, LOCK_UN);
         }else{
             print('ファイルロックに失敗しました');
         }
     }
-
     fclose($fp);
 }
-
-?>
-</body>
+    ?>
+  </body>
 </html>
