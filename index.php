5abeb9fbcda212d23a8289c9fddd2a56cbe3b6d7
Glitch (glitch-hello-website) <none>
2021-08-22 04:37:14 +0000

    🌧💸 Checkpoint ./index.php:34681680/455 ./style.css:34681680/475

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
   <head>
     <meta charset="utf-8">
     <title>掲示板</title>
-    <link rel="stylesheet" src="https://telk.glitch.me/style.css">
-    <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
+    <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
     <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-      <input type="text" class="name" name="name" value="" placeholder="コメントを入力">
-      <textarea placeholder="" spellcheck="false"></textarea>
+      <input type="text" class="name bdr" name="name" value="" placeholder="名前を入力"><br>
+      <textarea placeholder="コメントを入力" spellcheck="false" name="text" class="text bdr"></textarea><br>
+      <input type="submit" value="">
     </form>
   </body>
+  <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
 </html>
