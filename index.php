a1d96a72c2270e8026e24d0b806a2583889a46ca
Glitch (glitch-hello-website) <none>
2021-08-22 03:03:13 +0000

    🌮🎀 Checkpoint ./jp/jppl.txt:34681680/769 ./index.php:34681680/771

--- index.php
+++ index.php
@@ -4,26 +4,23 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Urbanist&display=swap" rel="stylesheet">
-    <title>Telk</title>
+    <title>Telkフォーラム</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
     <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
   </head>
   <body class="body">
     <div>
-      <h1 style="font-size:200%;">Make great discussion, with Telk.</h1>
-      <h3>Creation begins with discussion</h3>
+      <h1 style="font-size:200%;">Telkフォーラム</h1>
+      <h3>議論から雑談まで</h3>
       <div style="margin:10%;"></div>
-      <h1>All Servers</h1>
       <ul>
-        <li><a href="https://telk.glitch.me/en/" class="logo"><span style="font-size:200%;">Telk</span> English</a></li>
-        <li><a href="https://telk.glitch.me/jp/" class="logo"><span style="font-size:200%;">Telk</span> 日本語</a></li>
-        <li><a href="https://telk.glitch.me/ch/" class="logo"><span style="font-size:200%;">Telk</span> 中文</a></li>
+        <li><a href="https://telk.glitch.me/jp/" class="logo"><span style="font-size:200%;">Telk</span> メイン</a></li>
       </ul>
       <div style="margin:10%;"></div>
-      <h1>What is Telk?</h1>
+      <h1>Telkとは？</h1>
       <div class="section">
-        Telk is a very simple community. Registration is not required. It's completely free!
+        Telkは
       </div>
       <h1>Telk's source codes</h1>
       <div class="section">
