bc426672a45bc2c8180b70dc54409002aa8416da
Glitch (glitch-hello-website) <none>
2021-08-22 04:48:45 +0000

    🐈💝 Checkpoint ./index.php:34681680/510 ./style.css:34681680/233

--- index.php
+++ index.php
@@ -5,10 +5,29 @@
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+      writeData();
+    }
+    
+    readData();
+    
+    function readData(){
+      $log_file = "log.txt";
+      
+      $fp = fopen(log_file,"rb");
+      
+      if($fp){
+        if(flock($fp, LOCK_SH)){
+          
+        }
+      }
+    }
+    ?>
     <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
       <input type="text" class="name bdr" name="name" value="" placeholder="名前を入力"><br>
       <textarea placeholder="コメントを入力" spellcheck="false" name="text" class="text bdr"></textarea><br>
-      <input type="submit" value="">
+      <input type="submit" value="投稿する" class="in">
     </form>
   </body>
   <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
