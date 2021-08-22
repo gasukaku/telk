6ef035b737b91d6a19b14d9a0f41d2be46409926
Glitch (glitch-hello-website) <none>
2021-08-22 03:49:23 +0000

    👌🐊 Checkpoint ./main.php:34681680/114 ./style.css:34681680/1465 ./index.php:34681680/2547

--- index.php
+++ index.php
@@ -1,32 +1,8 @@
-
 <html>
   <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Urbanist&display=swap" rel="stylesheet">
-    <title>Telkフォーラム</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://telk.glitch.me/style.css">
+    <title>掲示板</title>
+    <link rel="stylesheet" src="https://telk.glitch.me">
     <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
   </head>
-  <body class="body">
-    <div class="section">
-      <h1 style="font-size:200%;">Telkフォーラム</h1>
-      <h3>議論から雑談まで</h3>
-      <div style="margin:10%;"></div>
-      <ul>
-        <li><a href="https://telk.glitch.me/" class="logo"><span style="font-size:200%;">Telk</span> 検索（開発中）</a></li>
-      </ul>
-      <div style="margin:10%;"></div>
-      <h1>Telkとは？</h1>
-        <p>
-          Telkは真面目な議論からほのぼのした雑談までできるフォーラムです。
-        </p>
-      <h1>Telkのソースコード</h1>
-        Telkのソースコードは基本的にオープンソースです。
-        ご自由にお使いください。
-        ソースコードは<a href="https://glitch.com/edit/#!/telk">こちら</a>。
-      <div style="margin:10%;"></div>
-    </div>
-  </body>
 </html>
