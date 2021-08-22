2cc409d33a782bf5e20b46f6ecbbc16280be2fae
Glitch (glitch-hello-website) <none>
2021-08-22 05:00:00 +0000

    📮🎅 Checkpoint ./index.php:34681680/992

--- index.php
+++ index.php
@@ -19,8 +19,31 @@
       
       if($fp){
         if(flock($fp, LOCK_SH)){
+          while(!feof($fp)){
+            $buffer = fgets($fp);
+            print($buffer);
+          }
           
+          flock($fp,LOCK_UN);
+        }else{
+          print("Fail to file lock!");
         }
+        }
+      }
+    }
+    function writeData(){
+      $name = $_POST["name"];
+      $text = $_POST["text"];
+      
+      $data = $data."<div class='post'><p class='namespace'>投稿者：".$name."</p>";
+      $data = $data."<p class='textspace'>".$text."</p></div>";
+      
+      $log_file = "log.txt";
+      
+      $fp = fopen($log_file,"ab");
+      
+      if($fp){
+        
       }
     }
     ?>
