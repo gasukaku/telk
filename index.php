0807f3faa4433872fa778a440fba7cb74f83bb74
Glitch (glitch-hello-website) <none>
2021-08-22 05:12:03 +0000

    😇😜 Checkpoint ./index.php:34681680/10299 ./style.css:34681680/323 ./log.txt:34681680/2

--- index.php
+++ index.php
@@ -5,52 +5,71 @@
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
-    <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-      writeData();
-    }
-    
-    readData();
-    
-    function readData(){
-      $log_file = "log.txt";
-      
-      $fp = fopen(log_file,"rb");
-      
-      if($fp){
-        if(flock($fp, LOCK_SH)){
-          while(!feof($fp)){
-            $buffer = fgets($fp);
-            print($buffer);
-          }
-          
-          flock($fp,LOCK_UN);
+    <div class="posts">
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    writeData();
+}
+
+readData();
+
+function readData(){
+    $log_file = 'log.txt';
+
+    $fp = fopen($log_file, 'rb');
+
+    if ($fp){
+        if (flock($fp, LOCK_SH)){
+            while (!feof($fp)) {
+                $buffer = fgets($fp);
+                print($buffer);
+            }
+
+            flock($fp, LOCK_UN);
         }else{
-          print("Fail to file lock!");
+            print('ファイルロックに失敗しました');
         }
+    }
+
+    fclose($fp);
+}
+
+function writeData(){
+    $personal_name = $_POST['personal_name'];
+    $contents = $_POST['contents'];
+
+    $data = "<hr>\r\n";
+    $data = $data."<div class='post'><p class='postname'>投稿者:".$personal_name."</p>\r\n";
+    $data = $data."<div class='line'></div>\r\n";
+    $data = $data."<p class='postbody'>".$contents."</p></div>";
+
+    $log_file = 'log.txt';
+
+    $fp = fopen($log_file, 'ab');
+
+    if ($fp){
+        if (flock($fp, LOCK_EX)){
+            if (fwrite($fp,  $data) === FALSE){
+                print('ファイル書き込みに失敗しました');
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
         }
-      }
     }
-    function writeData(){
-      $name = $_POST["name"];
-      $text = $_POST["text"];
-      
-      $data = $data."<div class='post'><p class='namespace'>投稿者：".$name."</p>";
-      $data = $data."<p class='textspace'>".$text."</p></div>";
-      
-      $log_file = "log.txt";
-      
-      $fp = fopen($log_file,"ab");
-      
-      if($fp){
-        
-      }
-    }
-    ?>
+
+    fclose($fp);
+}
+
+?>
+    </div>
     <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-      <input type="text" class="name bdr" name="name" value="" placeholder="名前を入力"><br>
-      <textarea placeholder="コメントを入力" spellcheck="false" name="text" class="text bdr"></textarea><br>
-      <input type="submit" value="投稿する" class="in">
+      <input type="text" name="personal_name"><br><br>
+      <textarea name="contents" wrap="off" spellcheck="false">
+      </textarea><br><br>
+      <input type="submit" value="投稿する">
     </form>
   </body>
   <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
