6ff5bfeedea63e0ec769582ea00781a7bbd60ea4
Glitch (telk) <none>
2021-09-01 09:05:17 +0000

    🚟🎄 Checkpoint ./index.php:34681680/2479 ./test.php:34681680/30 ./iio.js:34681680/238

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Telk利用規約</title>
+    <link rel="stylesheet" href="https://myfuncs.glitch.me/funcs/djunk.css">
+  </head>
+  <body style="padding:5%; padding-right:50%;">
+    <p class="djunk section title">Welcome to Telk</p>
+    <a class="djunk text" href="https://telk.glitch.me/chat.php" style="text-decoration:none; color:blue;">
+      Telk - 掲示板に移動する
+    </a>
+  </body>
+  <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
+</html>
