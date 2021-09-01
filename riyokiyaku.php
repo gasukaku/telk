ed48223cb8adaa42b221621fe44f0865fe2a550e
Glitch (telk) <none>
2021-09-01 08:53:14 +0000

    🌮👍 Checkpoint ./riyokiyaku.php:34681680/1184

--- riyokiyaku.php
+++ riyokiyaku.php
@@ -6,6 +6,7 @@
   </head>
   <body style="padding:5%; padding-right:50%;">
     <p class="djunk section title">利用規約</p>
+    <p class="djunk section text">この利用規約は、Telk及び系列のアプリケーション全てに適用されるものとします。</p>
     <p class="djunk section">責任について</p>
     <p class="djunk text">
       投稿された内容については、Telk開発チームは一切責任を負いません。何もかも自己責任でお願いします。
@@ -16,12 +17,18 @@
     </p>
     <p class="djunk section">個人情報について</p>
     <p class="djunk text">
-      「個人」を「特定」できる情報以外はOKです。例えば、住んでいる地域・都道府県・性別・
+      「個人」を「特定」できる情報以外はOKです。例えば、住んでいる地域・都道府県・性別・年齢などはOKですが、それ以外は一切許可していません。
+      なお、これに関しても自己責任です。投稿した場合は、最悪警察のお世話になることもあり得ます。
     </p>
     <p class="djunk section">ID・アカウントの投稿について</p>
     <p class="djunk text">
       一切許可していません。そのような投稿を見かけた場合は、無視してください。（運営がちょくちょく削除します）
     </p>
+    <p class="djunk section">Telkについて</p>
+    <p class="djunk text">
+      Telk及び系列のアプリケーションは、予告なく閉鎖される場合があります。
+    </p>
+    <p class="djunk section text">利用規約は以上です。</p>
   </body>
   <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
 </html>
