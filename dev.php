41f7c14811210a46855c32e185a970ac19179612
Glitch (telk) <none>
2021-09-02 12:26:37 +0000

    🌊👑 Checkpoint ./dev.php:34681680/1094

--- dev.php
+++ dev.php
@@ -1,18 +1,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title><?php print $dev[$_GET["name"]]; ?></title>
+    <title>Developers</title>
     <link rel="stylesheet" href="https://myfuncs.glitch.me/funcs/djunk.css">
   </head>
   <body style="padding:5%; padding-right:50%;">
-    <p class="djunk section title">
-      <?php
-      $dev = [
-        "まろさん"
-      ];
-      echo $dev[$_GET["name"]]
-      ?>
-    </p>
+    <p class="djunk section title"><?php echo $_GET["name"]; ?></p>
+    <p class="djunk text"><?php echo $_GET["body"]; ?></p>
   </body>
   <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
 </html>
