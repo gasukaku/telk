90a183068e07e5d6ceaf0845d2c1db7e56a700d8
Glitch (telk) <none>
2021-09-03 08:35:43 +0000

    🏑👑 Checkpoint ./login/index.php:34681680/413 ./login/style.css:34681680/448

--- login/index.php
+++ login/index.php
@@ -6,8 +6,13 @@
     <link rel="stylesheet" href="https://telk.glitch.me/login/style.css">
   </head>
   <body>
+    <?php
+    if($_GET["score"] == ""){ ?>
     <h1>Login</h1>
-    <input type="text" class="bot" value="" placeholder="Username">
-    <input type="text" class="bot" value="" placeholder="Password">
+    <input type="text" class="bot" value="" placeholder="Username"><br>
+    <input type="text" class="bot" value="" placeholder="Password"><br>
+    <input type="button" class="boty" value="Login">
+    <?php }elseif($_GET["score"] == "success"){ ?> 
+    <?php } ?>
   </body>
 </html>
