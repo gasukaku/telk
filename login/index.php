369728390b5218e94787edfc6e45ba1e4a1ce03b
Glitch (telk) <none>
2021-09-03 08:24:08 +0000

    💅🎎 Checkpoint ./login/index.php:34681680/775 ./login/style.css:34681680/313

---
+++ login/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://telk.glitch.me/login/style.css">
+  </head>
+  <body>
+    <h1>Login</h1>
+    <input type="text" class="bot" value="" placeholder="Username">
+    <input type="text" class="bot" value="" placeholder="Password">
+  </body>
+</html>
