235884d5458a57846d77ae08c3b82a1694bf15e7
Glitch (telk) <none>
2021-09-04 05:28:06 +0000

    🚍⭐️ Checkpoint ./style.css:34681680/241 ./chat.php:34681680/395 ./chatlog/.txt:34681680/2 ./guide.php:34681680/4

--- chat.php
+++ chat.php
@@ -1,13 +1,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Telk platform</title>
+    <title><?php print($_GET["room"]); ?></title>
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
-    <?php
-  echo "ルーム：".$_GET["room"]."\n";
-    ?>
+    <h1><?php echo $_GET["room"]."\n"; ?></h1>
     <div class="postView">
 <?php
 
@@ -66,8 +64,8 @@ function writeData(){
 ?>
     </div>
     <form method="POST" action="<?php print($_SERVER['https://telk.glitch.me/chat.php?room='.$_GET[room]]) ?>">
-      <input class="sendName" type="text" spellcheck="false" name="personal_name" placeholder="ニックネームを入力" required><br><br>
-      <textarea class="sendBody" name="contents" wrap="off" spellcheck="false" placeholder="投稿内容を入力" required></textarea><br><br>
+      <input class="sendName" type="text" spellcheck="false" name="personal_name" placeholder="ニックネームを入力" required><br>
+      <textarea class="sendBody" name="contents" wrap="off" spellcheck="false" placeholder="投稿内容を入力" required></textarea><br>
       <input type="submit" class="sendButton" value="利用規約に同意して投稿する"> <a href="https://telk.glitch.me/guide.php">利用規約の確認</a>
     </form>
   </body>
--- guide.php
+++ guide.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Telk利用規約</title>
+    <title>利用規約</title>
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
