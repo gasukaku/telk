3f368dbde0d7558ce6709c21584b40d151b9326c
Glitch (telk) <none>
2021-09-04 10:57:09 +0000

    🚐📼 Checkpoint ./main/chat.php:34681680/7

--- main/chat.php
+++ main/chat.php
@@ -66,7 +66,7 @@ function writeData(){
     <form method="POST" action="<?php print($_SERVER['https://telk.glitch.me/chat.php?room='.$_GET[room]]) ?>">
       <input class="sendName" type="text" spellcheck="false" name="personal_name" placeholder="ニックネーム" required><br>
       <textarea class="sendBody" name="contents" wrap="off" spellcheck="false" placeholder="投稿内容" required></textarea><br>
-      <input type="submit" class="sendButton" value="利用規約に同意して投稿する"> <a href="https://telk.glitch.me/guide.php">利用規約の確認</a>
+      <input type="submit" class="sendButton" value="利用規約に同意して投稿する"> <a href="https://telk.glitch.me/main/guide.php">利用規約の確認</a>
     </form>
     <a class="a plus" href="https://telk.glitch.me/main/">メインページへ戻る</a>
   </body>
