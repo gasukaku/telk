0c95a45410f53fb643d6e3e699654caf25d74691
Glitch (telk) <none>
2021-09-04 05:05:01 +0000

    🍪🌓 Checkpoint ./style.css:34681680/984 ./index.php:34681680/43

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://telk.glitch.me/style.css">
   </head>
   <body>
-    <a href="https://www.free-counter.jp/"><img src="https://www.f-counter.net/j/46/1630584352/" alt="アクセスカウンター"></a>
+    <img src="https://www.f-counter.net/j/46/1630584352/" alt="アクセスカウンター">
     <script type="text/javascript" src="https://telk.glitch.me/script.js"></script>
   </body>
 </html>
